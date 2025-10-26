6e2fd4a6d8edfcf7b795571e88bc5f37d7dc70fe
ssnsii <sithunissanka18@gmail.com>
2025-10-26 08:37:38 +0530

    Delete monitor.py

--- monitor.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from scipy.stats import ks_2samp
-
-def check_model_drift(new_preds, old_labels):
-    ks_stat, p_value = ks_2samp(new_preds, old_labels)
-    if p_value < 0.05:
-        print("Model drift detected!")
-    else:
-        print("No significant drift.")
